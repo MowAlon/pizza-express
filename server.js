2e2bc7e61b95a609115c2da48d9b913643954caf
Alon Waisman <mowalon@yahoo.com>
2016-01-06 22:42:08 -0700

    Completed tutorial

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const path = require('path')
+const generateId = require('./lib/generate-id')
+
+app.use(express.static('static'))
+app.use(bodyParser.urlencoded({extended:true}))
+// app.use(bodyParser.json())
+
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Pizza Express'
+app.locals.pizzas = {}
+
+app.set('view engine', 'jade')
+
+///// ROUTES /////
+app.get('/', (request, response) => {
+  response.render('index')
+})
+
+app.get('/pizzas/:id', (request, response) => {
+  var pizza = app.locals.pizzas[request.params.id]
+  response.render('pizza', {pizza: pizza})
+})
+
+app.post('/pizzas', (request, response) => {
+  if (!request.body.pizza) {return response.sendStatus(400)}
+  var id = generateId()
+  app.locals.pizzas[id] = request.body
+  response.redirect('/pizzas/' + id)
+})
+///// ROUTES /////
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+  })
+}
+
+module.exports = app
